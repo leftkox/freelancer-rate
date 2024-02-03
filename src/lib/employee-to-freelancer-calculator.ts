997fb3711f2d33fd5ecd9cc2993e4823c0aea02e
Lefteris Koxias <lefteris.koxias@envizage.me>
2024-02-03 09:21:58 +0200

    Add rate support

--- src/lib/employee-to-freelancer-calculator.ts
+++ src/lib/employee-to-freelancer-calculator.ts
@@ -1,22 +1,20 @@
-export const employeeToFreelancer = (netSalary: number, efkaClass: number): number => {
+export const employeeToFreelancer = (netSalary: number, efkaClass: number, fromCurrency: 'GBP' | 'EUR', toCurrency: 'GBP' | 'EUR'): number => {
+	const EFKA_CLASS_AMOUNTS = [142.93, 238.22, 285.87, 342.59, 411.78, 493.46, 642.06];
 
-  const EFKA_CLASS_AMOUNTS = [
-    142.93,
-    238.22,
-    285.87,
-    342.59,
-    411.78,
-    493.46,
-    642.06
-  ];
+	const GP_TAX_RATE = 0.22;
 
-  const GP_TAX_RATE = 0.22;
+	const ACCOUNTANT_SALARY = 600;
 
-  const ACCOUNTANT_SALARY = 600;
+	const taxableAmount = netSalary / (1 - GP_TAX_RATE);
 
-  const taxableAmount = netSalary / (1 - GP_TAX_RATE);
+	const annualGross = taxableAmount + ACCOUNTANT_SALARY + 12 * EFKA_CLASS_AMOUNTS[efkaClass];
 
-  const annualGross = taxableAmount + ACCOUNTANT_SALARY + EFKA_CLASS_AMOUNTS[efkaClass];
+	let rate = 1;
+	if (fromCurrency !== toCurrency) {
+		rate = Math.pow(0.85604041, fromCurrency === 'EUR' ? 1 : -1);
+	}
+	return rate * annualGross / 230;
+};
 
-  return annualGross / 230;
-}
+export const EFKA_CLASS_AMOUNTS = [142.93, 238.22, 285.87, 342.59, 411.78, 493.46, 642.06];
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,65 +1,37 @@
 <script lang="ts">
-  import Paper, { Content, Title, Subtitle } from '@smui/paper';
-  import Tab, { Label } from '@smui/tab';
-  import TabBar from '@smui/tab-bar';
-  import Textfield from '@smui/textfield';
-  import Button from '@smui/button';
-  import HelperText from '@smui/textfield/helper-text';
-  import Icon from '@smui/textfield/icon';
-	import { employeeToFreelancer } from '$lib/employee-to-freelancer-calculator';
- 
-  let active = 'Employee to freelancer';
+	import Paper, { Content } from '@smui/paper';
+	import Tab, { Label } from '@smui/tab';
+	import TabBar from '@smui/tab-bar';
+	import EmployeeToFreelancer from '$components/employee-to-freelancer.svelte';
 
-  let netSalary = 0;
+	let active = 'Employee to freelancer';
 
-  let efkaClass = 0;
-
-  let dailyRate: number | undefined = undefined;
-  const calculateEmployeeToFreelancer = () => {
-    dailyRate = employeeToFreelancer(netSalary ?? 0, efkaClass ?? 0);
-  }
 </script>
 
-<style>
-  .form-label {
-    margin-bottom: 10px;
-  }
-</style>
+<div class="container">
+<Paper >
+	<TabBar let:tab bind:active tabs={['Employee to freelancer', 'Freelancer to employee']}>
+		<Tab {tab}>
+			<Label>{tab}</Label>
+		</Tab>
+	</TabBar>
 
-<div>
-  <TabBar let:tab bind:active tabs={['Employee to freelancer', 'Freelancer to employee']}>
-    <Tab {tab}>
-      <Label>{tab}</Label>
-    </Tab>
-  </TabBar>
-
-  {#if active === 'Employee to freelancer'}
-    <Paper variant="unelevated">
-      <div class="form-label mdc-typography--body1">Enter your net annual salary</div>
-      <Textfield variant="outlined" bind:value={netSalary} label="Salary">
-      </Textfield>
-
-      <div style="margin-top: 30px;" class="form-label mdc-typography--body1">Enter your social security class (0 for "fresh" freelancer)</div>
-      <Textfield variant="outlined" bind:value={efkaClass} label="EFKA Class">
-      </Textfield>
-
-      <div style="margin-top: 30px;">
-      <Button on:click={() => calculateEmployeeToFreelancer()} variant="raised">
-        <Label>Calculate</Label>
-      </Button>
-
-      {#if dailyRate !== undefined}
-      <div style="margin-top: 20px">
-        <Title>Your equivalent daily rate (in the same currency) is:</Title>
-        <Subtitle>{Math.round(dailyRate)}</Subtitle>
-      </div>
-      {/if}
-    </div>
-    </Paper>
-
-  {:else if active === 'Freelancer to employee'}
-  <Paper variant="unelevated">
-    <Content>Under construction!!!</Content>
-  </Paper>
-  {/if}
+	{#if active === 'Employee to freelancer'}
+		<EmployeeToFreelancer />
+	{:else if active === 'Freelancer to employee'}
+		<div >
+			<Content>Under construction!!!</Content>
+		</div>
+	{/if}
+	</Paper>
 </div>
+
+<style>
+	.container {
+		width: 60%;
+		margin: auto;
+		min-width: 300px;
+		max-width: 550px;
+		padding-top: 50px;
+	}
+</style>
